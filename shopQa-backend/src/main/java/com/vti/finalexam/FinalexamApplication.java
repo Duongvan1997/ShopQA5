9be58a1f82ac24fa5be623f12a9521e3464a4d50
alice1311 <pdinh0823@gmail.com>
2024-06-22 13:45:10 +0700

    update order

--- shopQa-backend/src/main/java/com/vti/finalexam/FinalexamApplication.java
+++ shopQa-backend/src/main/java/com/vti/finalexam/FinalexamApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalexamApplication {
+public class cd FinalexamApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FinalexamApplication.class, args);
 	}
